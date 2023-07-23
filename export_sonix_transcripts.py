22903a104ff5f1a6272f5e4c03948697c5aacd2a
Anthony T <tokyographer@gmail.com>
2023-07-23 13:49:01 +0200

    Corrected indentation error

--- export_sonix_transcripts.py
+++ export_sonix_transcripts.py
@@ -21,10 +21,17 @@ def export_transcripts():
     # Export transcripts one by one and save them as text files
     for transcript_id in tqdm(transcript_ids, desc="Exporting Transcripts"):
         response = requests.get(f"{SONIX_API_URL}/v1/files/{transcript_id}/content", headers=headers)
-        transcript_data = json.loads(response.text)
-        filename = f"transcripts/{transcript_data['data']['name']}.txt"
-        with open(filename, "w", encoding="utf-8") as file:
-            file.write(transcript_data["data"]["content"])
+
+        # Check if the API request was successful
+         if response.status_code == 200:
+            transcript_data = json.loads(response.text)
+            filename = f"transcripts/{transcript_data['data']['name']}.txt"
+            with open(filename, "w", encoding="utf-8") as file:
+                    file.write(transcript_data["data"]["content"])
+        else:
+            print(f"Error exporting transcript {transcript_id}. Status Code: {response.status_code}")
+            print("Response content:", response.text)
+
 
 if __name__ == "__main__":
     start_time = time.time()
